25f6857a03226b41499846437d5570241e2389df
hearteiei <supalit1@gmail.com>
2021-01-08 17:19:41 +0700

    lab9.2 v.completed

--- lab9_2.cpp
+++ lab9_2.cpp
@@ -9,9 +9,12 @@ int main (){
 	source.open("cheerbook.txt") ;
 	dest.open("cheerbook_copy.txt");
 	
-	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
-	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
-
+	 string textline;
+ dest << "-------------------- BOOM ---------------------\n";
+ while(getline(source,textline)){
+	 dest << textline <<"\n";
+ }
+dest << "-------------------- HA!! ---------------------\n";
     source.close();
     dest.close();
 	return 0;
